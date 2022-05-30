5cfabdfa05784664ab11049bff513c8ed16ba41e
Joao Marco <joaomarcobarros@gmail.com>
2022-05-29 22:03:47 -0300

    Added auxiliar methods to update state in the agreement contract

--- Agreement.sol
+++ Agreement.sol
@@ -20,6 +20,26 @@ contract Agreement{
     /// The function cannot be called at the current state
     error InvalidState();
 
+    ///Only the seller can call this function
+    error OnlySeller();
+
+    ///Only the buyer can call this function
+    error OnlyBuyer();
+
+    modifier onlySeller(){
+        if(msg.sender != seller){
+            revert OnlySeller();
+        }
+        _;
+    }
+
+    modifier onlyBuyer(){
+        if(msg.sender != buyer){
+            revert OnlyBuyer();
+        }
+        _;
+    }
+
     modifier inState(State state_){
         if(state != state_){
             revert InvalidState();
@@ -31,6 +51,27 @@ contract Agreement{
         require(msg.value >= (2*value), "Please send in 2x the purchase amount");
         buyer = payable(msg.sender);
         state = State.Locked;
+    }
+
+    function confirmReceived() external onlyBuyer inState(State.Locked){
+        state = State.Released;
+        buyer.transfer(value);
+    }
+
+    function paySeller() external onlySeller inState(State.Released){
+        state = State.Inactive;
+        seller.transfer(address(this).balance);
+    }
+
+    function abort() external onlySeller inState(State.Created){
+        state = State.Inactive;
+        seller.transfer(address(this).balance);
+    }
+
+    function withdraw() external onlySeller inState(State.Locked){
+        state = State.Inactive;
+        buyer.transfer(2*value);
+        seller.transfer(address(this).balance);
     }
 
 
