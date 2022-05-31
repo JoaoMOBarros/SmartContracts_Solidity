3d1764de8123b10744143e6db01500f1cbb41245
Joao Marco <joaomarcobarros@gmail.com>
2022-05-31 20:21:29 -0300

    Created User Contract

---
+++ RiskReview/User.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import './Report.sol';
+
+contract User{
+
+    enum Activity{Vote, PeerReviewAccepted, PeerReviewRejected, ReportAccepeted, ReportRejected}
+
+    struct ActivityReceipt{
+        Activity activity;
+        uint timestamp;
+    }
+
+    address public owner;
+    uint createdAt;
+    Report[] reports;
+    ActivityReceipt[] activities;
+    int public reputation;
+
+    constructor(){
+        owner = msg.sender;
+        createdAt = block.timestamp;
+        reputation = 0;
+    }
+
+    function getActivityReputation(Activity activity_) private pure returns (int score){
+        if(activity_ == Activity.Vote) return 1;
+        if(activity_ == Activity.PeerReviewAccepted) return 5 ;
+        if(activity_ == Activity.PeerReviewRejected) return -1 ;
+        if(activity_ == Activity.ReportAccepeted) return 7 ;
+        if(activity_ == Activity.ReportRejected) return -2 ;
+    }
+
+    function addActivity(uint8 activity_) external {
+        ActivityReceipt memory receipt = ActivityReceipt(Activity(activity_), block.timestamp);
+        activities.push(receipt);
+        reputation += getActivityReputation(Activity(activity_));
+    }
+
+    function getAllUserReposts() public view returns(Report[] memory){
+        return reports;
+    }
+
+    function getAllUserActivities() public view returns(ActivityReceipt[] memory){
+        return activities;
+    }
+
+}
